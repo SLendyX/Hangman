0e09398f4cd6d67ff18ba0d9803ab427361f2aed
SLendyX <slendyx2002@gmail.com>
2024-05-08 19:14:06 +0300

    Finished guess letter mechanic, added menu functionality and added invalid input catcher

--- Hangman/Form1.cs
+++ Hangman/Form1.cs
@@ -11,6 +11,8 @@ using static System.Net.Mime.MediaTypeNames;
 using System.Xml.Linq;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
+
 
 namespace Hangman
 {
@@ -18,22 +20,23 @@ namespace Hangman
     {
 
         //declare variables
-        public int i = 0;
+        public int frameIndex = 0;
         public string[] Frame =
         {
-            "D:\\Documents\\visual studio\\repos\\Hangman\\images\\frame_1.png",
-            "D:\\Documents\\visual studio\\repos\\Hangman\\images\\frame_2.png",
-            "D:\\Documents\\visual studio\\repos\\Hangman\\images\\frame_3.png",
-            "D:\\Documents\\visual studio\\repos\\Hangman\\images\\frame_4.png",
-            "D:\\Documents\\visual studio\\repos\\Hangman\\images\\frame_5.png",
-            "D:\\Documents\\visual studio\\repos\\Hangman\\images\\frame_6.png",
-            "D:\\Documents\\visual studio\\repos\\Hangman\\images\\frame_7.png",
+            ".\\images\\frame_1.png",
+            ".\\images\\frame_2.png",
+            ".\\images\\frame_3.png",
+            ".\\images\\frame_4.png",
+            ".\\images\\frame_5.png",
+            ".\\images\\frame_6.png",
+            ".\\images\\frame_7.png",
         };
 
-        public StreamReader sr = new StreamReader("D:\\Documents\\visual studio\\repos\\Hangman\\input\\cuvinte.txt");
         public string Word;
         public string hiddenWord;
 
+        public int PozYStart { get => startBtn.Location.Y; set => PozYStart = value; }
+        public int PozYQuit { get => quitBtn.Location.Y; set => PozYQuit = value; }
         public Form1()
         {
             InitializeComponent();
@@ -42,8 +45,96 @@ namespace Hangman
    
         private void GuessBtn_Click(object sender, EventArgs e)
         {
-           
+            Check_Letter_in_Word();
         }
+
+        private void Input_Enter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                Check_Letter_in_Word();
+            }
+        }
+        
+        private void Check_Letter_in_Word()
+        {
+            errorLabel.Visible = false;
+
+            string letters = input.Text;
+            input.Text = string.Empty;
+
+            char letter;
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letter = letters[i];
+                if(letter >= 'A' && letter <= 'Z' || letter >= 'a' && letter <= 'z')
+                {
+
+                    if (letter >= 'A' && letter <= 'Z')
+                        letter += ' ';
+
+                    if (!Is_Guessed_Letter(letter))
+                    {
+                        if (Word.ToLower().IndexOf(letter) < 0)
+                        {
+                            guessedLetters.AppendText(letter.ToString() + " ");
+                            LoadFrame(Frame[frameIndex++]);
+                        }
+                        else
+                        {
+                            Unhide_Letter(letter);
+                        }
+
+                        if (frameIndex == Frame.Length)
+                        {
+                            Game_Over();
+                            break;
+                        }
+                        else if (Has_Won())
+                        {
+                            Game_Won();
+                        }
+                    }
+                }
+                else
+                {
+                    errorLabel.Text = "*Input must be a valid letter [A-Za-z]";
+                    errorLabel.Visible = true;
+                    break;
+                }
+            }
+        }
+
+        private void Unhide_Letter(char letter)
+        {
+            if (letter >= 'A' && letter <= 'Z')
+                letter += ' ';
+
+
+            string letters = Word.ToLower().Replace(letter, '\t');
+            char[] temp = hiddenWord.ToCharArray();
+            for(int i=0; i<letters.Length; i++)
+            {
+                if (letters[i] == '\t')
+                {
+                    temp[i*2] = letter;
+                }
+            }
+            hiddenWord = new string(temp);
+            guessedWord.Text = hiddenWord;
+        }
+
+        private bool Is_Guessed_Letter(char letter)
+        {
+            string content = guessedLetters.Text;
+            for(int i=0; i<content.Length; i += 2)
+            {
+                if (content[i] == letter) return true;
+            }
+            return false;
+        }
+
 
         private void Intialize_Game()
         {
@@ -52,15 +143,20 @@ namespace Hangman
             input.Visible = false;  
             guessedWord.Visible = false;
             mainFrame.Visible = false;
+            guessedLetters.Visible = false;
+            guessedLettersLabel.Visible = false;
+            menuBtn.Visible = false;
+            continueBtn.Visible = false;
+            errorLabel.Visible = false;
+
             startBtn.Visible = true;
             quitBtn.Visible = true;
-            Word = string.Empty;
-            hiddenWord = string.Empty;
-    }
+           
+        }
 
-        private void LoadFrame(string Frame)
+        private void LoadFrame(string frame)
         {
-            mainFrame.ImageLocation = Frame;
+            mainFrame.ImageLocation = frame;
             mainFrame.Load();
         }
 
@@ -70,30 +166,131 @@ namespace Hangman
             input.Visible = true;
             guessedWord.Visible = true;
             mainFrame.Visible = true;
+            guessedLettersLabel.Visible = true;
+            guessedLetters.Visible = true;
+            menuBtn.Visible = true;
+
             startBtn.Visible = false;
             quitBtn.Visible = false;
-            LoadFrame(Frame[i++]);
+
+            input.Text = string.Empty;
+            guessedLetters.Text = string.Empty;
+
+
+            LoadFrame(Frame[frameIndex++]);
             Word = Get_Random_Word();
             hiddenWord = Hide_Word(Word);
+          
+
             guessedWord.Text = hiddenWord;
         }
+
+        private bool Has_Won()
+        {
+            for(int i=0; i< hiddenWord.Length; i += 2)
+            {
+                if (hiddenWord[i] != Word[i/2])
+                    return false;
+            }
+            return true;
+        }
+
+        private void Game_Won()
+        {
+            frameIndex = 0;
+            gameOverText.Visible = true;
+            gameOverText.Text = "You Won!";
+            guessBtn.Visible = false;
+            input.Visible = false;
+            mainFrame.Visible = false;
+            guessedLetters.Visible = false;
+            guessedLettersLabel.Visible = false;
+            menuBtn.Visible = false;
+
+            startBtn.Text = "New Game";
+            startBtn.Visible = true;
+            quitBtn.Visible = true;
+        }
+
+        private void Game_Over()
+        {
+            frameIndex = 0;
+            gameOverText.Visible = true;
+            gameOverText.Text = "Game Over!";
+            guessBtn.Visible = false;
+            input.Visible = false;
+            mainFrame.Visible = false;
+            guessedLetters.Visible = false;
+            guessedLettersLabel.Visible = false;
+            menuBtn.Visible = false;
+
+            startBtn.Text = "Try again";
+            startBtn.Visible = true;
+            quitBtn.Visible = true;
+
+
+            char[] answer = new char[Word.Length * 2];
+
+            for(int i=0; i<Word.Length; i++)
+            {
+                answer[2 * i] = Word[i];
+                answer[2 * i + 1] = ' ';
+            }
+            guessedWord.Text = "Answer: " + new string(answer);
+        }
+
         private void Quit(object sender, EventArgs e)
         {
             System.Windows.Forms.Application.Exit();
         }
 
+        private void Menu_Click(object sender, EventArgs e)
+        {
+            gameOverText.Visible = false;
+            guessBtn.Visible = false;
+            input.Visible = false;
+            guessedWord.Visible = false;
+            mainFrame.Visible = false;
+            guessedLetters.Visible = false;
+            guessedLettersLabel.Visible = false;
+            menuBtn.Visible = false;
+            startBtn.Visible = true;
+            quitBtn.Visible = true;
+            continueBtn.Visible = true;
+
+            startBtn.Text = "New Game";
+
+        }
+
+        private void Continue_Click(object sender, EventArgs e)
+        {
+            gameOverText.Visible = true;
+            guessBtn.Visible = true;
+            input.Visible = true;
+            guessedWord.Visible = true;
+            mainFrame.Visible = true;
+            guessedLetters.Visible = true;
+            guessedLettersLabel.Visible = true;
+            menuBtn.Visible = true;
+
+            startBtn.Visible = false;
+            quitBtn.Visible = false;
+            continueBtn.Visible = false;
+        }
+
+
+
         private string Get_Random_Word()
         {
+            StreamReader sr = new StreamReader(".\\input\\cuvinte.txt");
             string word = sr.ReadLine();
 
             int lineNumber = 0;
             Random rd = new Random();
             int index = rd.Next(0, 2308);
-            
+
             while(word != null)
             {
-               // Debug.AppendText(word);
-               // Debug.AppendText("\n");
 
                 if (lineNumber == index)
                     return word;
@@ -106,10 +303,10 @@ namespace Hangman
         private string Hide_Word(string word)
         {
             char[] chars = word.ToCharArray();
-            char[] newchars = new char[chars.Length*2];
+            char[] newchars = new char[chars.Length * 2];
             int j = 0;
 
-            for(int i=0; i< word.Length; i++)
+            for (int i = 0; i < word.Length; i++)
             {
                 if (chars[i] != ' ')
                 {
@@ -120,6 +317,5 @@ namespace Hangman
 
             return new string(newchars);
         }
-
     }
 }
