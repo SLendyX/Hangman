35d69cc8ef2957474e0bd3a4e3758f961f911ff0
SLendyX <slendyx2002@gmail.com>
2024-05-14 22:40:49 +0300

    added load question, need to figure out load answer functionality and checkbox function

--- Hangman/Form1.Designer.cs
+++ Hangman/Form1.Designer.cs
@@ -31,48 +31,26 @@ namespace Hangman
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.input = new System.Windows.Forms.TextBox();
-            this.guessBtn = new System.Windows.Forms.Button();
+            this.submitBtn = new System.Windows.Forms.Button();
             this.gameOverText = new System.Windows.Forms.TextBox();
             this.startBtn = new System.Windows.Forms.Button();
             this.quitBtn = new System.Windows.Forms.Button();
-            this.guessedLetters = new System.Windows.Forms.TextBox();
-            this.guessedLettersLabel = new System.Windows.Forms.Label();
-            this.guessedWord = new System.Windows.Forms.TextBox();
             this.menuBtn = new System.Windows.Forms.Button();
-            this.errorLabel = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.continueBtn = new System.Windows.Forms.Button();
             this.mainFrame = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.questionLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.mainFrame)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // input
+            // submitBtn
             // 
-            this.input.AcceptsReturn = true;
-            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.input.Location = new System.Drawing.Point(342, 481);
-            this.input.MaxLength = 64;
-            this.input.Name = "input";
-            this.input.Size = new System.Drawing.Size(138, 35);
-            this.input.TabIndex = 0;
-            this.input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Input_Enter);
-            // 
-            // guessBtn
-            // 
-            this.guessBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guessBtn.Location = new System.Drawing.Point(361, 550);
-            this.guessBtn.Name = "guessBtn";
-            this.guessBtn.Size = new System.Drawing.Size(98, 35);
-            this.guessBtn.TabIndex = 1;
-            this.guessBtn.Text = "Guess";
-            this.guessBtn.UseVisualStyleBackColor = true;
-            this.guessBtn.Click += new System.EventHandler(this.GuessBtn_Click);
+            this.submitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitBtn.Location = new System.Drawing.Point(361, 550);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(98, 35);
+            this.submitBtn.TabIndex = 1;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
             // 
             // gameOverText
             // 
@@ -89,7 +67,7 @@ namespace Hangman
             // 
             // startBtn
             // 
-            this.startBtn.Font = new System.Drawing.Font("Dutch801 XBd BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.startBtn.Location = new System.Drawing.Point(342, 165);
             this.startBtn.Name = "startBtn";
             this.startBtn.Size = new System.Drawing.Size(138, 73);
@@ -100,7 +78,7 @@ namespace Hangman
             // 
             // quitBtn
             // 
-            this.quitBtn.Font = new System.Drawing.Font("Dutch801 XBd BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.quitBtn.Location = new System.Drawing.Point(342, 244);
             this.quitBtn.Name = "quitBtn";
             this.quitBtn.Size = new System.Drawing.Size(138, 72);
@@ -108,38 +86,6 @@ namespace Hangman
             this.quitBtn.Text = "Quit";
             this.quitBtn.UseVisualStyleBackColor = true;
             this.quitBtn.Click += new System.EventHandler(this.Quit);
-            // 
-            // guessedLetters
-            // 
-            this.guessedLetters.Enabled = false;
-            this.guessedLetters.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guessedLetters.Location = new System.Drawing.Point(56, 357);
-            this.guessedLetters.MaxLength = 13;
-            this.guessedLetters.Multiline = true;
-            this.guessedLetters.Name = "guessedLetters";
-            this.guessedLetters.Size = new System.Drawing.Size(93, 32);
-            this.guessedLetters.TabIndex = 8;
-            this.guessedLetters.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // guessedLettersLabel
-            // 
-            this.guessedLettersLabel.AutoSize = true;
-            this.guessedLettersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guessedLettersLabel.Location = new System.Drawing.Point(26, 313);
-            this.guessedLettersLabel.Name = "guessedLettersLabel";
-            this.guessedLettersLabel.Size = new System.Drawing.Size(154, 25);
-            this.guessedLettersLabel.TabIndex = 9;
-            this.guessedLettersLabel.Text = "Guessed letters:";
-            // 
-            // guessedWord
-            // 
-            this.guessedWord.Enabled = false;
-            this.guessedWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guessedWord.Location = new System.Drawing.Point(301, 440);
-            this.guessedWord.Name = "guessedWord";
-            this.guessedWord.Size = new System.Drawing.Size(220, 35);
-            this.guessedWord.TabIndex = 10;
-            this.guessedWord.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // menuBtn
             // 
@@ -154,49 +100,9 @@ namespace Hangman
             this.menuBtn.UseVisualStyleBackColor = true;
             this.menuBtn.Click += new System.EventHandler(this.Menu_Click);
             // 
-            // errorLabel
-            // 
-            this.errorLabel.AutoSize = true;
-            this.errorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.errorLabel.ForeColor = System.Drawing.Color.Red;
-            this.errorLabel.Location = new System.Drawing.Point(298, 519);
-            this.errorLabel.Name = "errorLabel";
-            this.errorLabel.Size = new System.Drawing.Size(85, 17);
-            this.errorLabel.TabIndex = 13;
-            this.errorLabel.Text = "PlaceHolder";
-            this.errorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(320, 325);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 13);
-            this.label1.TabIndex = 14;
-            this.label1.Text = "Question";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(0, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 15;
-            this.label2.Text = "label2";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(279, 353);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(23, 21);
-            this.button1.TabIndex = 16;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // continueBtn
             // 
-            this.continueBtn.BackgroundImage = global::Hangman.Properties.Resources._22651ee4_d928_4bae_a4f3_d35dd4aa178c_1;
-            this.continueBtn.Font = new System.Drawing.Font("Dutch801 XBd BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.continueBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.continueBtn.Location = new System.Drawing.Point(342, 86);
             this.continueBtn.Name = "continueBtn";
             this.continueBtn.Size = new System.Drawing.Size(138, 73);
@@ -213,13 +119,14 @@ namespace Hangman
             this.mainFrame.TabIndex = 2;
             this.mainFrame.TabStop = false;
             // 
-            // pictureBox1
+            // questionLabel
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(517, 63);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(343, 326);
-            this.pictureBox1.TabIndex = 17;
-            this.pictureBox1.TabStop = false;
+            this.questionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionLabel.Location = new System.Drawing.Point(73, 319);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(596, 79);
+            this.questionLabel.TabIndex = 13;
+            this.questionLabel.Text = "label1";
             // 
             // Form1
             // 
@@ -227,51 +134,33 @@ namespace Hangman
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(860, 648);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.questionLabel);
             this.Controls.Add(this.continueBtn);
             this.Controls.Add(this.menuBtn);
-            this.Controls.Add(this.guessedWord);
-            this.Controls.Add(this.guessedLettersLabel);
-            this.Controls.Add(this.guessedLetters);
             this.Controls.Add(this.quitBtn);
             this.Controls.Add(this.startBtn);
             this.Controls.Add(this.gameOverText);
             this.Controls.Add(this.mainFrame);
-            this.Controls.Add(this.guessBtn);
-            this.Controls.Add(this.input);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.submitBtn);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Hangman";
             ((System.ComponentModel.ISupportInitialize)(this.mainFrame)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox input;
-        private System.Windows.Forms.Button guessBtn;
+        private System.Windows.Forms.Button submitBtn;
         private System.Windows.Forms.PictureBox mainFrame;
         private System.Windows.Forms.TextBox gameOverText;
         private System.Windows.Forms.Button startBtn;
         private System.Windows.Forms.Button quitBtn;
-        private System.Windows.Forms.TextBox guessedLetters;
-        private System.Windows.Forms.Label guessedLettersLabel;
-        private TextBox guessedWord;
         private Button menuBtn;
         private Button continueBtn;
-        private Label errorLabel;
-        private Label label1;
-        private Label label2;
-        private Button button1;
-        private PictureBox pictureBox1;
+        private Label questionLabel;
     }
 }
 
--- Hangman/Form1.cs
+++ Hangman/Form1.cs
@@ -31,9 +31,6 @@ namespace Hangman
             ".\\images\\frame_6.png",
             ".\\images\\frame_7.png",
         };
-
-        public string Word;
-        public string hiddenWord;
         
         public class Question
         {
@@ -73,17 +70,14 @@ namespace Hangman
             }
         }
 
-        public List<Question> Questions = new List<Question>();
-        
-        
-
-
+        public List<Question> Questions = new List<Question>();       
+        public int listIndex = 0;
+        public bool[] answers = new bool[9];
 
         public Form1()
         {
             InitializeComponent();
             Intialize_Game();
-            InitializeList();
         }
    
         private void InitializeList()
@@ -110,8 +104,26 @@ namespace Hangman
                 question.SetAnswers(answers);
             }
 
+            foreach (Question question in Questions)
+            {
+                string line = sr.ReadLine();
+                string[] lineToArray = line.Split(' ');
+                bool[] bools = new bool[lineToArray.Length];
 
-            sr.Close();
+
+                for (int i = 0; i < lineToArray.Length; i++)
+                {
+                    int temp = Convert.ToInt32(lineToArray[i]);
+                    bools[i] = Convert.ToBoolean(temp);
+                }
+                
+                question.SetCorrectAnswers(bools);
+            }
+
+
+
+
+                sr.Close();
             
 
             
@@ -128,8 +140,144 @@ namespace Hangman
                 }
             }
         }
-        
 
+
+        private void Intialize_Game()
+        {
+            InitializeList();
+
+            gameOverText.Visible = false;
+            submitBtn.Visible = false;
+            mainFrame.Visible = false;
+            questionLabel.Visible = false;
+
+            menuBtn.Visible = false;
+            continueBtn.Visible = false;
+
+
+            startBtn.Visible = true;
+            quitBtn.Visible = true;
+           
+        }
+
+        private void LoadFrame(string frame)
+        {
+            mainFrame.ImageLocation = frame;
+            mainFrame.Load();
+        }
+
+        private void LoadQuestion(Question question)
+        {
+            questionLabel.Text = question.GetQuestion()+":";
+        }
+
+        private void LoadAnswers(Question question)
+        {
+            int x = questionLabel.Location.X;
+            int y = questionLabel.Location.Y;
+            string[] answers = question.GetAnswers();
+            for (int i = 0; i < answers.Length; i++)
+            {
+                CheckBox cb = new CheckBox();
+                cb.Text = answers[i];
+                cb.Location = new Point(x, y + 30 * i);
+                this.Controls.Add(cb);
+            }
+        }
+
+
+        private void Start_Game(object sender, EventArgs e)
+        {
+            frameIndex = 0;
+            submitBtn.Visible = true;
+            questionLabel.Visible = true;
+
+            mainFrame.Visible = true;
+
+            menuBtn.Visible = true;
+
+            startBtn.Visible = false;
+            quitBtn.Visible = false;
+            continueBtn.Visible = false;
+
+            LoadQuestion(Questions[listIndex]);
+            LoadAnswers(Questions[listIndex++]);
+            LoadFrame(Frame[frameIndex++]);
+        }
+
+
+
+
+
+
+        private void Game_Won()
+        {
+            gameOverText.Visible = true;
+            gameOverText.Text = "You Won!";
+            submitBtn.Visible = false;
+
+            mainFrame.Visible = false;
+
+            menuBtn.Visible = false;
+
+            startBtn.Text = "New Game";
+            startBtn.Visible = true;
+            quitBtn.Visible = true;
+        }
+
+        private void Game_Over()
+        {
+            gameOverText.Visible = true;
+            gameOverText.Text = "Game Over!";
+            submitBtn.Visible = false;
+
+            mainFrame.Visible = false;
+
+            menuBtn.Visible = false;
+
+            startBtn.Text = "Try again";
+            startBtn.Visible = true;
+            quitBtn.Visible = true;
+
+        }
+
+        private void Quit(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void Menu_Click(object sender, EventArgs e)
+        {
+            gameOverText.Visible = false;
+            submitBtn.Visible = false;
+
+            mainFrame.Visible = false;
+
+            menuBtn.Visible = false;
+            startBtn.Visible = true;
+            quitBtn.Visible = true;
+            continueBtn.Visible = true;
+
+            startBtn.Text = "New Game";
+
+        }
+
+        private void Continue_Click(object sender, EventArgs e)
+        {
+            gameOverText.Visible = true;
+            submitBtn.Visible = true;
+
+            mainFrame.Visible = true;
+
+            menuBtn.Visible = true;
+
+            startBtn.Visible = false;
+            quitBtn.Visible = false;
+            continueBtn.Visible = false;
+        }
+
+
+/*
         private void GuessBtn_Click(object sender, EventArgs e)
         {
             Check_Letter_in_Word();
@@ -143,7 +291,7 @@ namespace Hangman
                 Check_Letter_in_Word();
             }
         }
-        
+
         private void Check_Letter_in_Word()
         {
             errorLabel.Visible = false;
@@ -155,7 +303,7 @@ namespace Hangman
             for (int i = 0; i < letters.Length; i++)
             {
                 letter = letters[i];
-                if(letter >= 'A' && letter <= 'Z' || letter >= 'a' && letter <= 'z')
+                if (letter >= 'A' && letter <= 'Z' || letter >= 'a' && letter <= 'z')
                 {
 
                     if (letter >= 'A' && letter <= 'Z')
@@ -201,11 +349,11 @@ namespace Hangman
 
             string letters = Word.ToLower().Replace(letter, '\t');
             char[] temp = hiddenWord.ToCharArray();
-            for(int i=0; i<letters.Length; i++)
+            for (int i = 0; i < letters.Length; i++)
             {
                 if (letters[i] == '\t')
                 {
-                    temp[i*2] = letter;
+                    temp[i * 2] = letter;
                 }
             }
             hiddenWord = new string(temp);
@@ -215,156 +363,11 @@ namespace Hangman
         private bool Is_Guessed_Letter(char letter)
         {
             string content = guessedLetters.Text;
-            for(int i=0; i<content.Length; i += 2)
+            for (int i = 0; i < content.Length; i += 2)
             {
                 if (content[i] == letter) return true;
             }
             return false;
-        }
-
-
-        private void Intialize_Game()
-        {
-            
-
-            gameOverText.Visible = false;
-            guessBtn.Visible = false;
-            input.Visible = false;  
-            guessedWord.Visible = false;
-            mainFrame.Visible = false;
-            guessedLetters.Visible = false;
-            guessedLettersLabel.Visible = false;
-            menuBtn.Visible = false;
-            continueBtn.Visible = false;
-            errorLabel.Visible = false;
-
-            startBtn.Visible = true;
-            quitBtn.Visible = true;
-           
-        }
-
-        private void LoadFrame(string frame)
-        {
-            mainFrame.ImageLocation = frame;
-            mainFrame.Load();
-        }
-
-        private void Start_Game(object sender, EventArgs e)
-        {
-            frameIndex = 0;
-            guessBtn.Visible = true;
-            input.Visible = true;
-            guessedWord.Visible = true;
-            mainFrame.Visible = true;
-            guessedLettersLabel.Visible = true;
-            guessedLetters.Visible = true;
-            menuBtn.Visible = true;
-
-            startBtn.Visible = false;
-            quitBtn.Visible = false;
-            continueBtn.Visible = false;
-
-            input.Text = string.Empty;
-            guessedLetters.Text = string.Empty;
-
-
-            LoadFrame(Frame[frameIndex++]);
-            Word = Get_Random_Word();
-            hiddenWord = Hide_Word(Word);
-          
-
-            guessedWord.Text = hiddenWord;
-        }
-
-        private bool Has_Won()
-        {
-            for(int i=0; i< hiddenWord.Length; i += 2)
-            {
-                if (hiddenWord[i] != Word[i/2])
-                    return false;
-            }
-            return true;
-        }
-
-        private void Game_Won()
-        {
-            gameOverText.Visible = true;
-            gameOverText.Text = "You Won!";
-            guessBtn.Visible = false;
-            input.Visible = false;
-            mainFrame.Visible = false;
-            guessedLetters.Visible = false;
-            guessedLettersLabel.Visible = false;
-            menuBtn.Visible = false;
-
-            startBtn.Text = "New Game";
-            startBtn.Visible = true;
-            quitBtn.Visible = true;
-        }
-
-        private void Game_Over()
-        {
-            gameOverText.Visible = true;
-            gameOverText.Text = "Game Over!";
-            guessBtn.Visible = false;
-            input.Visible = false;
-            mainFrame.Visible = false;
-            guessedLetters.Visible = false;
-            guessedLettersLabel.Visible = false;
-            menuBtn.Visible = false;
-
-            startBtn.Text = "Try again";
-            startBtn.Visible = true;
-            quitBtn.Visible = true;
-
-
-            char[] answer = new char[Word.Length * 2];
-
-            for(int i=0; i<Word.Length; i++)
-            {
-                answer[2 * i] = Word[i];
-                answer[2 * i + 1] = ' ';
-            }
-            guessedWord.Text = "Answer: " + new string(answer);
-        }
-
-        private void Quit(object sender, EventArgs e)
-        {
-            System.Windows.Forms.Application.Exit();
-        }
-
-        private void Menu_Click(object sender, EventArgs e)
-        {
-            gameOverText.Visible = false;
-            guessBtn.Visible = false;
-            input.Visible = false;
-            guessedWord.Visible = false;
-            mainFrame.Visible = false;
-            guessedLetters.Visible = false;
-            guessedLettersLabel.Visible = false;
-            menuBtn.Visible = false;
-            startBtn.Visible = true;
-            quitBtn.Visible = true;
-            continueBtn.Visible = true;
-
-            startBtn.Text = "New Game";
-
-        }
-
-        private void Continue_Click(object sender, EventArgs e)
-        {
-            gameOverText.Visible = true;
-            guessBtn.Visible = true;
-            input.Visible = true;
-            guessedWord.Visible = true;
-            mainFrame.Visible = true;
-            guessedLetters.Visible = true;
-            guessedLettersLabel.Visible = true;
-            menuBtn.Visible = true;
-
-            startBtn.Visible = false;
-            quitBtn.Visible = false;
-            continueBtn.Visible = false;
         }
 
 
@@ -412,6 +415,6 @@ namespace Hangman
             }
 
             return new string(newchars);
-        }
+        }*/
     }
 }
